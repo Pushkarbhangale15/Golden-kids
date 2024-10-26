e98d10205e3cc01b4255cfa2b3fc31228b063243
Pushkar Harishchandra Bhangale <pushkarbhangale15@gmail.com>
2024-10-26 09:33:51 +0530

    Some final commits for a responsive navbar

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -35,5 +35,4 @@ const About = ({ setPlayState }) => {
     </div>
   );
 };
-
 export default About
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,19 +1,29 @@
 import React from 'react'
 import './Hero.css'
 import white_arrow from '../../assets/white-arrow.png'
-
+import { Link } from 'react-scroll'
 const Hero = () => {
   return (
-    <div className='hero container'>
-        <div className="hero-text">
-            
+    <div className="hero container">
+      <div className="hero-text">
         <h1>Nurturing Young Minds with Care and Creativity</h1>
-        <p>We foster a love for learning from the start. Our pre-primary programs spark curiosity, build confidence, and nurture each child's unique talents in a joyful, safe environment. </p>
-
-        <button className='btn'>Explore More <img src={white_arrow} alt="" /></button>
-        </div>
+        <p>
+          We foster a love for learning from the start. Our pre-primary programs
+          spark curiosity, build confidence, and nurture each child's unique
+          talents in a joyful, safe environment.{" "}
+        </p>
+        <Link
+          to="programs"
+          smooth={true}
+          offset={-300}
+          duration={500}
+          className="btn"
+        >
+          Explore More <img src={white_arrow} alt="" />
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Hero
